2796412abfa893dc8f8066a836be5c3c1f0f98e1
manjulasathyam1 <manjula.sathyam@reach24.net>
2023-04-12 12:51:35 +0530

    categories-checkout-page

---
+++ src/Form.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import image from './images/Lillakortet.png'
+import { useForm } from 'react-hook-form';
+
+export default function Form() {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm()
+    const onSubmit = data => console.log(data);
+
+    
+  return (
+    <section>
+        <div className="register">
+            <div className="col-1">
+                <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
+                    <h2 className="title">Shipping Details</h2>
+
+                    <label className="firstname">FirstName: </label>
+                    <input type="text" placeholder='John' />
+
+                    <label className="lastname">LastName: </label>
+                    <input type="text" placeholder='S' />
+                    <label className="address">Address: </label>
+                    <input type="text" placeholder='Uppsala, Swedan' />
+                    <label className="postalcode">Postal Code</label>
+                    <input type="text" placeholder='123 987' />
+                    <button className='btn'>Submit</button>
+                </form>
+                
+                <h2>My Cart</h2>
+                <div className="mycart">
+                    <img className="product-img" src={image} alt="" />
+                    <h5 className="product-title">"Little Adventure Card"</h5>
+                    <h5 className="price"> "549.00kr"</h5>
+                    
+                    <img className="product-img" src={image} alt="" />
+                    <h5 className="product-title1">"Stora klatterkortet"</h5>
+                    <h5 className="price"> "1,549.00kr"</h5>
+                </div>
+                <h5>Total:  2100.00kr </h5>
+                <button className="btn-success">Edit Cart</button>
+
+            </div>
+        </div>
+    </section>
+  )
+}
